4cc7592c8b3f9bcdad55b3fd0c5b1d2165e1ac1f
Fr4zT <81241725+Fr4zT@users.noreply.github.com>
2022-01-29 19:14:22 +0100

    Added planet

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -7,6 +7,7 @@ public class Controller : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] float maxY;
     [SerializeField] float minY;
+    [SerializeField] float stopY;
     [SerializeField] ParticleSystem smoke;
     ParticleSystem.MainModule mainSmoke;
     [SerializeField] Transform rocket;
@@ -35,18 +36,37 @@ public class Controller : MonoBehaviour
         {
             mainSmoke.simulationSpeed = Mathf.Abs(movement) * speed;
             if (movement > 0) smoke.Emit(5);
-            transform.position += new Vector3(0, movement * speed * Time.deltaTime, 0);
 
-            var curY = transform.position.y;
+            var curY = transform.position.y + movement * speed * Time.deltaTime;
 
             if (curY > minY && curY < maxY) rocket.localEulerAngles += new Vector3(0, movement * speed, 0);
-            transform.position = new Vector3(0, Mathf.Clamp(curY, minY, maxY), 0);
 
             mCamera.backgroundColor = skyColor.Evaluate(curY / maxY);
+            if (curY > stopY)
+            {
+                mCamera.transform.parent = null;
+                if (curY > stopY + 10)
+                {
+                    rocket.transform.localPosition = new Vector3(0, 0.059f, -3.2f);
+                    var cameraPos = mCamera.transform.position;
+                    mCamera.transform.position = Vector3.MoveTowards(cameraPos, new Vector3(cameraPos.x, transform.position.y, cameraPos.z), 1);
+                }
+                else
+                {
+                    rocket.transform.localPosition = new Vector3(0, 0.059f, 2);
+                }
+            }
+            else
+            {
+                mCamera.transform.parent = transform;
+            }
+            transform.position += new Vector3(0, curY, 0);
+            transform.position = new Vector3(0, Mathf.Clamp(curY, minY, maxY), transform.position.z);
         }
-
     }
 
-
-    
+    //IEnumerator MoveCamera()
+    //{
+    //    while(mCamera.transform.position!=)
+    //}
 }
